acadaf8ce9472f17546bfb3a5a73915069bbfbc7
Md Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2019-10-14 14:24:05 +0600

    add category view in sidebar

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -18,30 +18,19 @@
                   <h5 class="card-header">Categories</h5>
                   <div class="card-body">
                       <div class="row">
-                          <div class="col-lg-6">
+                          <div class="col-lg-12">
                               <ul class="list-unstyled mb-0">
-                                  <li>
-                                      <a href="#">Web Design</a>
-                                  </li>
-                                  <li>
-                                      <a href="#">HTML</a>
-                                  </li>
-                                  <li>
-                                      <a href="#">Freebies</a>
-                                  </li>
-                              </ul>
-                          </div>
-                          <div class="col-lg-6">
-                              <ul class="list-unstyled mb-0">
-                                  <li>
-                                      <a href="#">JavaScript</a>
-                                  </li>
-                                  <li>
-                                      <a href="#">CSS</a>
-                                  </li>
-                                  <li>
-                                      <a href="#">Tutorials</a>
-                                  </li>
+                                  <?php
+                                    $sql = "SELECT * FROM `tbl_category`";
+                                    $result = $db->query($sql);
+                                    while ($row = $result->fetch_assoc()) {
+                                        ?>
+                                      <li>
+                                          <a href="showByCat.php?cat_id=<?php echo $row['cat_id']; ?>"><?php echo $row['cat_name']; ?></a>
+                                      </li>
+                                      <hr>
+                                  <?php }
+                                    ?>
                               </ul>
                           </div>
                       </div>
