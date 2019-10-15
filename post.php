7f8a8aa441c86b3f5bacc178b05a440dda90858b
Md Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2019-10-15 11:44:25 +0600

    updated coment form

--- post.php
+++ post.php
@@ -67,31 +67,40 @@
                 <p class="lead"> <?php echo $row['post_content']; ?> </p>
 
                 <hr>
-            <?php }
-            ?>
-            <!-- Comments Form -->
-            <div class="card my-4">
-                <h5 class="card-header">Leave a Comment:</h5>
-                <div class="card-body">
-                    <form>
-                        <div class="form-group">
-                            <textarea class="form-control" rows="3"></textarea>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
+                <!-- Comments Form -->
+                <div class="card my-4">
+                    <h5 class="card-header">Leave a Comment:</h5>
+                    <div class="card-body">
+                        <form>
+                            <div class="form-group">
+                                <input type="text" name="comment_body" class="form-control" placeholder="Enter your name..">
+                            </div>
+                            <div class="form-group">
+                                <textarea class="form-control" rows="3"></textarea>
+                            </div>
+                            <input type="hidden" name="post_id" value="<?php echo $id; ?>">
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
                 </div>
-            </div>
 
-            <!-- Single Comment -->
-            <div class="media mb-4">
-                <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
-                <div class="media-body">
-                    <h5 class="mt-0">Commenter Name</h5>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras
-                    purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi
-                    vulputate fringilla. Donec lacinia congue felis in faucibus.
-                </div>
-            </div>
+                <!-- Single Comment -->
+                <?php
+                    $sql = "SELECT * FROM `tbl_comment` WHERE `comment_id` = $id";
+                    $result = $db->query($sql);
+                    while ($row = $result->fetch_assoc()) {
+                        ?>
+                    <div class="media mb-4">
+                        <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
+                        <div class="media-body">
+                            <h5 class="mt-0"><?php echo $row['username']; ?></h5>
+                            <?php echo $row['comment_body']; ?>
+                        </div>
+                    </div>
+                    <hr>
+            <?php }
+            }
+            ?>
         </div>
         <!-- Sidebar Widgets Column -->
         <?php include "includes/sidebar.php"; ?>
