8dc853b0a09fb4da172137959647fd4f5673ea1f
Md Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2019-10-16 18:23:46 +0600

    edit category refacor started

--- admin/editCategory.php
+++ admin/editCategory.php
@@ -1,4 +1,5 @@
-<?php ob_start(); include "inc/header.php"; ?>
+<?php ob_start();
+include "inc/header.php"; ?>
 <div id="wrapper">
     <!-- Sidebar -->
     <?php include "inc/sidebar.php"; ?>
@@ -30,43 +31,49 @@
                 </div>
                 <div class="card-body">
                     <?php
+                    if (isset($_SESSION['errMsg'])) { ?>
+                        <div class="<?php echo $_SESSION['errMsgClass'] ?>" id="showMsg">
+                            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                            <strong><?php echo $_SESSION['errMsg'] ?></strong></div>
+                    <?php
+                        unset($_SESSION["errMsgClass"]);
+                        unset($_SESSION["errMsg"]);
+                    }
+                    ?>
+                    <?php
                     if (isset($_GET['id']) && is_numeric($_GET['id'])) {
                         $id = $_GET['id'];
                         $sql = "SELECT * FROM `tbl_category` WHERE `cat_id` = $id";
                         $result = $db->query($sql) or die($db->error);;
                         $row = $result->fetch_assoc();
                         ?>
-                    <form action="sReq/editCategory.php" method="post" id="editCat">
-                        <div class="form-group">
-                            <label for="catTitle">Category Title</label>
-                            <input type="text" name="cat_name" class="form-control"
-                                value="<?php echo $row['cat_name']; ?>" id="catTitle" placeholder="Category Name">
-                        </div>
+                        <form action="sReq/editCategory.php" method="post" id="editCat">
+                            <div class="form-group">
+                                <label for="catTitle">Category Title</label>
+                                <input type="text" name="cat_name" class="form-control" value="<?php echo $row['cat_name']; ?>" id="catTitle" placeholder="Category Name">
+                            </div>
 
-                        <div class="form-group">
-                            <label for="catContent">Description</label>
-                            <textarea class="form-control" name="cat_description" id="catContent"
-                                rows="3"><?php echo $row['cat_description']; ?></textarea>
-                        </div>
-                        <div class="form-group">
-                            <b>Status: </b>
-                            <select name="cat_status" class="form-control">
-                                <option value="1">Publish</option>
-                                <option value="0">Draft</option>
-                            </select>
-                        </div>
-                        <input type="hidden" name="id" value="<?php echo $id; ?>">
-                        <div class="form-group row">
-                            <div class="col-sm-6">
-                                <button type="submit" name="editCategory"
-                                    class="btn btn-success btn-block btn-lg text-center p-3 m-2">Save Edit</button>
+                            <div class="form-group">
+                                <label for="catContent">Description</label>
+                                <textarea class="form-control" name="cat_description" id="catContent" rows="3"><?php echo $row['cat_description']; ?></textarea>
                             </div>
-                            <div class="col-sm-6">
-                                <button type="reset"
-                                    class="btn btn-secondary btn-block btn-lg text-center p-3 m-2">Reset</button>
+                            <div class="form-group">
+                                <b>Status: </b>
+                                <select name="cat_status" class="form-control">
+                                    <option value="1">Publish</option>
+                                    <option value="0">Draft</option>
+                                </select>
                             </div>
-                        </div>
-                    </form>
+                            <input type="hidden" name="id" value="<?php echo $id; ?>">
+                            <div class="form-group row">
+                                <div class="col-sm-6">
+                                    <button type="submit" name="editCategory" class="btn btn-success btn-block btn-lg text-center p-3 m-2">Save Edit</button>
+                                </div>
+                                <div class="col-sm-6">
+                                    <button type="reset" class="btn btn-secondary btn-block btn-lg text-center p-3 m-2">Reset</button>
+                                </div>
+                            </div>
+                        </form>
                     <?php } else {
                         header("Location: allCategory.php");
                     } ?>
