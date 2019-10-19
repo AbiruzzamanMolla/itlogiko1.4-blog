e45731c31cc475b779c69084aba5f9880c813afd
Md Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2019-10-19 12:17:46 +0600

    report link  in sidebar

--- admin/inc/sidebar.php
+++ admin/inc/sidebar.php
@@ -6,23 +6,6 @@
             <span>Dashboard</span>
         </a>
     </li>
-    <!-- Dropdown list -->
-    <!-- <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fas fa-fw fa-folder"></i>
-            <span>Pages</span>
-        </a>
-        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-            <h6 class="dropdown-header">Login Screens:</h6>
-            <a class="dropdown-item" href="login.html">Login</a>
-            <a class="dropdown-item" href="register.html">Register</a>
-            <a class="dropdown-item" href="forgot-password.html">Forgot Password</a>
-            <div class="dropdown-divider"></div>
-            <h6 class="dropdown-header">Other Pages:</h6>
-            <a class="dropdown-item" href="404.html">404 Page</a>
-            <a class="dropdown-item active" href="blank.html">Blank Page</a>
-        </div>
-    </li> -->
     <li class="nav-item">
         <a class="nav-link" href="allPost.php">
             <i class="fas fa-fw fa-chart-area"></i>
@@ -42,5 +25,10 @@
         <a class="nav-link" href="allUser.php">
             <i class="fas fa-fw fa-chart-area"></i>
             <span>All Users</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="reportGen.php">
+            <i class="fas fa-fw fa-chart-area"></i>
+            <span>Report Genarate</span></a>
     </li>
 </ul>
